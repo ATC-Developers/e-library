763166fea57e7a907a71b0e9193e91ebaa63fcb9
Shashank Sharma <shashank.sharma98@gmail.com>
2018-08-31 14:09:41 +0530

    Static file error fixed

--- portal/settings.py
+++ portal/settings.py
@@ -139,3 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
